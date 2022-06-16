8ec822cf57ec6dd29e81bc18424c2392fc46b2be
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-06-16 15:06:29 +0300

    monty

--- main.c
+++ main.c
@@ -1,20 +1,74 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "monty.h"
+#include "lists.h"
 
-void main (int argc, char *argv[])
+data_t data = DATA_INIT;
+
+/**
+ * monty - helper function for main function
+ * @args: pointer to struct of arguments from main
+ *
+ * Description: opens and reads from the file
+ * containing the opcodes, and calls the function
+ * that will find the corresponding executing function
+ */
+void monty(args_t *args)
 {
-	char ch;
-	FILE *file_ptr;
-	file_ptr = fopen(argv[1], "w");
-	printf("write sth to the file ... ");
-	
-	while ((ch=getchar()) != EOF)
-		putc(ch, file_ptr);
-	fclose(file_ptr);
+	size_t len = 0;
+	int get = 0;
+	void (*code_func)(stack_t **, unsigned int);
 
-	file_ptr = fopen(argv[1], "r");
-	printf("The text in the file is ... \n");
-	while ((ch=getchar()) != EOF)
-		printf("%c", ch);
-	fclose(file_ptr);
+	if (args->ac != 2)
+	{
+		dprintf(STDERR_FILENO, USAGE);
+		exit(EXIT_FAILURE);
+	}
+	data.fptr = fopen(args->av, "r");
+	if (!data.fptr)
+	{
+		dprintf(STDERR_FILENO, FILE_ERROR, args->av);
+		exit(EXIT_FAILURE);
+	}
+	while (1)
+	{
+		args->line_number++;
+		get = getline(&(data.line), &len, data.fptr);
+		if (get < 0)
+			break;
+		data.words = strtow(data.line);
+		if (data.words[0] == NULL || data.words[0][0] == '#')
+		{
+			free_all(0);
+			continue;
+		}
+		code_func = get_func(data.words);
+		if (!code_func)
+		{
+			dprintf(STDERR_FILENO, UNKNOWN, args->line_number, data.words[0]);
+			free_all(1);
+			exit(EXIT_FAILURE);
+		}
+		code_func(&(data.stack), args->line_number);
+		free_all(0);
+	}
+	free_all(1);
+}
+
+/**
+ * main - entry point for monty bytecode interpreter
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(int argc, char *argv[])
+{
+	args_t args;
+
+	args.av = argv[1];
+	args.ac = argc;
+	args.line_number = 0;
+
+	monty(&args);
+
+	return (EXIT_SUCCESS);
 }
